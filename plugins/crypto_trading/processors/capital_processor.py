ecd012e0b2362309bc89413bf47e518d7e770b90
mixycronico <mixycronico@aol.com>
2025-04-19 06:42:36 -0400

    Update capital_processor.py

--- plugins/crypto_trading/processors/capital_processor.py
+++ plugins/crypto_trading/processors/capital_processor.py
@@ -8,6 +8,7 @@ from corec.core import ComponenteBase, zstd, serializar_mensaje
 from ..utils.db import TradingDB
 from ..utils.helpers import CircuitBreaker
 import json
+import logging
 import asyncio
 from typing import Dict, Any
 from datetime import datetime, timedelta
