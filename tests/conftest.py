715b6979c38e55b04549ac7260f5bfb6d967fa0e
mixycronico <mixycronico@aol.com>
2025-04-20 09:51:30 -0400

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -58,17 +58,12 @@ def test_config():
     }
 
 
-@pytest.fixture(scope="function")
-async def nucleus(mock_redis, test_config, request):
+@pytest.fixture
+def nucleus(mock_redis, test_config):
     """Crea una instancia de CoreCNucleus para pruebas."""
     nucleus = CoreCNucleus("test_config.json")
     nucleus.config = test_config
     nucleus.redis_client = mock_redis
-
-    def teardown():
-        asyncio.run(nucleus.detener())
-
-    request.addfinalizer(teardown)
     return nucleus
 
 
