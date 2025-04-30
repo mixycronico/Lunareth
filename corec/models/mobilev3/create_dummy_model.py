a62b3c0ace8d90465c00be436d361ee40bcd4e9a
mixycronico <mixycronico@aol.com>
2025-04-30 09:55:37 -0400

    Update create_dummy_model.py

--- corec/models/mobilev3/create_dummy_model.py
+++ corec/models/mobilev3/create_dummy_model.py
@@ -3,11 +3,13 @@ import torch.nn as nn
 from torchvision.models import mobilenet_v3_small
 import os
 
+
 def create_dummy_model():
     model = mobilenet_v3_small(weights=None)
     model.classifier[-1] = nn.Linear(model.classifier[-1].in_features, 3)
     os.makedirs("corec/models/mobilev3", exist_ok=True)
     torch.save(model.state_dict(), "corec/models/mobilev3/model.pth")
+
 
 if __name__ == "__main__":
     create_dummy_model()
